68da464cc7d732febd9cc883463379c0b8656c46
Migmatic <lamandealexis@gmail.com>
2022-02-16 06:50:20 +0100

    Mardi

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("custom-slider");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
+
+
+
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 500){
-        document.getElementById("navbar").style.marginTop = "0";
+        document.getElementById("navbar").style.marginTop = "10px";
         document.getElementById("navbar").style.marginLeft = "1%";
         document.getElementById("imgnav").style.width = "100px";
     }
@@ -11,3 +36,4 @@ window.onscroll = function () {
 
     }
 }
+
